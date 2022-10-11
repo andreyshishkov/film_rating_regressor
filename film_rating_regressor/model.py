5c5c31c29d0c96832d26cf7544e08f06f6e30a55
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-10-11 17:26:11 +0300

    Add reqirements and fumction 'create_pipeline'

--- film_rating_regressor/model.py
+++ film_rating_regressor/model.py
@@ -6,18 +6,21 @@ import pandas as pd
 import pickle
 
 
-model = tf_idf_lr = Pipeline([
-    ('normalizer', Normalizer()),
-    ('tf-idf', TfidfVectorizer(preprocessor=' '.join)),
-    ('lr', SVR())
-])
+def create_pipeline():
+    pipeline = tf_idf_lr = Pipeline([
+        ('normalizer', Normalizer()),
+        ('tf-idf', TfidfVectorizer(preprocessor=' '.join)),
+        ('lr', SVR())
+    ])
+    return pipeline
+
 
 if __name__ == '__main__':
     data = pd.read_csv('../data/film_ratings_1.csv', delimiter=';')
     X = data['description']
     y = data['rating']
+    model = create_pipeline()
     model.fit(X, y)
 
     with open('../data/model.pkl', 'wb') as file:
         pickle.dump(model, file)
-        
